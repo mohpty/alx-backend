3f545156e4123e6f5b2de1baf914778aa2e8d7b2
Mohammad Fathi <mohfabdou@gmail.com>
2024-07-31 00:08:30 +0300

    real init

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+task 0
+'''
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    '''
+    server the index page
+    '''
+
+    return render_template('index.html')
